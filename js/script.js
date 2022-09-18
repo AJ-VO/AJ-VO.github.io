d76215a1af4bd725a8568ce463b6a96f435bdad2
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-09-18 13:44:05 -0400

    Show position in standings

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ fetch("https://aj-vo.github.io/jsons/players.json")
 .then(function(products){
     let placeholder = document.querySelector("#data-output");
     let out = "";
+    position = 1;
     for(let product of products){
         out+= `
             <tr>
-                <td>${product.name}</td>
+                <td>(${position})${product.name}</td>
                 <td>${product.elo}</td>
                 <td>${product.wins}</td>
                 <td>${product.losses}</td>
             </tr>
         `;
+        position = position + 1
     }
 
     placeholder.innerHTML = out;
@@ -34,7 +36,7 @@ fetch("https://aj-vo.github.io/jsons/results.json")
     for(let product of products){
         out+= `
             <tr>
-                <td>${product.date}<br><b>${product.winner}</b> (${product.winnerELO}) + ${product.winnerGain} d. <b>${product.loser}</b> (${product.loserELO}) - ${product.winnerGain} [${product.score}]</td>
+                <td>${product.date}<br><b>${product.winner}</b> (${product.winnerELO}) + ${product.winnerGain} d. <b>${product.loser}</b> (${product.loserELO}) - ${product.winnerGain} <br>[${product.score}]</td>
             </tr>
         `;
     }
