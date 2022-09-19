7515b3bb8d86144551839720563a80042c72b516
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-09-18 21:57:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -12,19 +12,23 @@ def main():
     #Rank Difference
     l_won = []#bool
     diff = []#int
+    temps_de_jeu_g = []#String
+    temps_de_jeu_p = []
     for i in range(1, worksheet.nrows):
         try:
             if int(worksheet.cell(i, 12).value) > int(worksheet.cell(i, 21).value):
                 l_won.append(True)
                 diff.append(int(worksheet.cell(i, 12).value)-int(worksheet.cell(i, 21).value))
+                temps_de_jeu_g.append(int(worksheet.cell(i, 26).value))
             else:
                 l_won.append(False)
+                temps_de_jeu_p.append(int(worksheet.cell(i, 26).value))
         except:
             continue
 
     print(l_won.count(True)/len(l_won))
-    print(max(diff))
-    print(min(diff))
+    print(statistics.mean(temps_de_jeu_g))
+    print(statistics.mean(temps_de_jeu_p))
 
 if __name__ == "__main__":
     main()
