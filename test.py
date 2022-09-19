789531d1a20e8212047e3ecdcd8a7efd092c5b26
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-09-19 13:41:29 -0400

    Update

--- test.py
+++ test.py
@@ -14,7 +14,15 @@ def main():
     diff = []#int
     temps_de_jeu_g = []#String
     temps_de_jeu_p = []
+    nationalite = []
+    first_serve_g = []
+    first_serve_p = []
     for i in range(1, worksheet.nrows):
+        nationalite.append(worksheet.cell(i, 10).value)
+        if worksheet.cell(i, 29).value > 0:
+            first_serve_g.append((worksheet.cell(i, 30).value/worksheet.cell(i, 29).value))#30 = AE
+        if worksheet.cell(i, 36).value > 0:
+            first_serve_p.append((worksheet.cell(i, 36).value/worksheet.cell(i, 36).value))#36 = AK
         try:
             if int(worksheet.cell(i, 12).value) > int(worksheet.cell(i, 21).value):
                 l_won.append(True)
@@ -25,10 +33,11 @@ def main():
                 temps_de_jeu_p.append(int(worksheet.cell(i, 26).value))
         except:
             continue
+        nationalite.append(worksheet.cell(i, 10).value)
 
-    print(l_won.count(True)/len(l_won))
-    print(statistics.mean(temps_de_jeu_g))
-    print(statistics.mean(temps_de_jeu_p))
+    print(statistics.mean(first_serve_p))
+
+    
 
 if __name__ == "__main__":
     main()
