d60a10397a2920f42bd3a3f60ee003264f738d42
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-10-12 15:50:58 -0400

    Update player.js

--- js/player.js
+++ js/player.js
@@ -15,7 +15,7 @@ fetch("https://aj-vo.github.io/jsons/players.json")
             winrate = product.wins/(product.wins+product.losses)*100
             winrate = Math.round(winrate * 10) / 10
             out += `
-            <img src="${product.img}" alt="Profile" style="width:100%"><h1>${product.name}</h1><p class="title">${product.elo}</p><p>${product.wins} Victoires - ${product.losses} Defaites (${winrate}%)</p><br><p>Sequence : ${product.streak}</p><br>
+            <img src="${product.img}" alt="Profile" style="width:100%"><h1>${product.name}</h1><p class="title">${product.elo}</p><p>${product.wins} Victoires - ${product.losses} Defaites (${winrate}%)</p><p>Sequence : ${product.streak}</p><br>
             `;
         }
     }
