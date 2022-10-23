afe59f07437289afca309f5efab03d529fe70bfa
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-10-23 00:04:42 -0400

    Update

--- admin/app.py
+++ admin/app.py
@@ -9,7 +9,7 @@ class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        ## Setting up Initial Things
+        ## Setting up initial things
         self.title("Gestion Loisirs Plus")
         self.geometry("720x550")
         self.resizable(True, True)
@@ -20,7 +20,6 @@ class App(tk.Tk):
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
 
         ## Initialize Frames
         self.frames = {}
@@ -49,16 +48,21 @@ class HomePage(tk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
+        #Home Labels
         label = tk.Label(self, text="GLP - Menu", font=('Times', '20'))
         label.pack(pady=0,padx=0)
         label1 = tk.Label(self, text=U.get_date(), font=('Times', '12'))
         label1.pack(pady=0,padx=0)
 
-        button = tk.Button(self, width=20, height=2, text="Créer une commande", font=('Times', '20'), command=lambda: parent.show_frame(parent.Create), bg='#6f9da1')
+        #Home Buttons
+        button = tk.Button(self, width=20, height=3, 
+            text="Créer une commande", font=('Times', '20'), command=lambda: parent.show_frame(parent.Create), bg='#6f9da1')
         button.pack(pady=10,padx=10)
-        button1 = tk.Button(self, width=20, height=2, text="Voir les commandes", font=('Times', '20'), command=lambda: parent.show_frame(parent.ShowOrders), bg='#6f9da1')
+        button1 = tk.Button(self, width=20, height=3, 
+            text="Voir les commandes", font=('Times', '20'), command=lambda: parent.show_frame(parent.ShowOrders), bg='#6f9da1')
         button1.pack(pady=10,padx=10)
-        button2 = tk.Button(self, width=20, height=2, text="Terminer une commande", font=('Times', '20'), command=lambda: parent.show_frame(parent.Create), bg='#6f9da1')
+        button2 = tk.Button(self, width=20, height=3, 
+            text="Terminer une commande", font=('Times', '20'), command=lambda: parent.show_frame(parent.Create), bg='#6f9da1')
         button2.pack(pady=10,padx=10)
 
         ## ADD CODE HERE TO DESIGN THIS PAGE
@@ -88,13 +92,13 @@ class HomePage(tk.Frame):
 
         return menubar
 
-#---------------------------------------- Validation PAGE FRAME / CONTAINER ------------------------------------------------------------------------
+#---------------------------------------- Validation PAGE FRAME / CONTAINER (MODEL PAGE) ------------------------------------------------------------------------
 
 class Validation(tk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        label = tk.Label(self, text="Validation Page", font=('Times', '20'))
+        label = tk.Label(self, text="Validation Test Page Page", font=('Times', '20'))
         label.pack(pady=0,padx=0)
 
         ## ADD CODE HERE TO DESIGN THIS PAGE
@@ -130,81 +134,55 @@ class Create(tk.Frame):
     def __init__(self, parent, container):
         super().__init__(container)
 
-        def submitRaket(clientName, price, name_entry, name_entry1,confirmation_label):
+        def submitRaket(clientName, price, name_entry, name_entry1, confirmation_label):
 
-            def goBack():
+            def quitCreation():
                 confirmation_label.config(text="")
                 button.pack_forget()
                 parent.show_frame(parent.HomePage)
 
-            #If name not given?
-            if str(clientName.get()) == "":
-                messagebox.showinfo('Erreur', "Missing Client Name")
-                return
-            #If price is negative or not a number?
-            try:
-                float(price.get())
-            except:
-                messagebox.showinfo('Erreur', "Price is NaN")
-                return
-            if float(price.get()) < 0:
-                messagebox.showinfo('Erreur', "Price is < 0$")
-                return
+            U.checkOrder(clientName.get(), price.get())
 
-            #Get order.json
-            orderJSON = U.get_orders()
-            orderDict = {}
-            #Generate random 6 digit ID
-            order_id = ""
-            for i in range(5):
-                tank = str(random.randint(0,9))
-                order_id = order_id+tank
-            #Get Date
-            date_id = U.get_date()
-
-            orderDict["client_name"] = clientName.get()
-            orderDict["order_id"] = order_id
-            orderDict["order_date"] = date_id
-            orderDict["price"] = price.get()
-            orderDict["payment_status"] = False
-            orderJSON.append(orderDict)
+            orderJSON, order_id= U.createOrder(clientName.get(), price.get())
 
             U.dump_orders(orderJSON)
 
             #Main menu
-            confirmationStr = "Commande créer!\nId: "+order_id+"\nNom: "+clientName.get()+"\nPrix: "+price.get()
-
+            global confirmationStr
+            confirmationStr = "Commande créer!\nId: "+order_id["id"]+"\nNom: "+clientName.get()+"\nPrix: "+price.get()
             #Update label
             confirmation_label.config(text = confirmationStr)
 
-            button = tk.Button(self, text="Menu Principal", font=('Times', '15'), command=lambda: goBack())
+            #Create Menu Button
+            button = tk.Button(self, text="Menu Principal", font=('Times', '15'), command=lambda: quitCreation())
             button.pack(pady=20,padx=20)
 
             #Delete Entry Text
             name_entry.delete(0, END)
             name_entry1.delete(0, END)
 
+        #Entry Variables
         name_var=tk.StringVar()
         passw_var=tk.StringVar()
 
-        label = tk.Label(self, text="Créer une commande!", font=('Times', '20'))
-        label.pack(pady=0,padx=0)
+        label = tk.Label(self, text="Créer une commande!", font=('Times', '20')).pack(pady=0,padx=0)
 
         name_label = tk.Label(self, text = 'Nom', font=('Times',20, 'bold'))
-        name_entry = tk.Entry(self, textvariable = name_var, font=('Times',20,'normal'))
         name_label.pack(pady=10,padx=10)
-        name_entry.pack(pady=0,padx=0)
+        name_entry = tk.Entry(self, textvariable = name_var, font=('Times',20,'normal'))
+        name_entry.pack(pady=10,padx=10)
 
         name_label1 = tk.Label(self, text = 'Prix Total (xx.xx ou xx)', font=('Times',20, 'bold'))
-        name_entry1 = tk.Entry(self, textvariable = passw_var, font=('Times',20,'normal'))
         name_label1.pack(pady=10,padx=10)
-        name_entry1.pack(pady=0,padx=0)
-
-        button = tk.Button(self, text="Submit", font=('Times', '20'), command=lambda: submitRaket(name_var, passw_var, name_entry, name_entry1, confirmation_label))
-        button.pack(pady=20,padx=20)
+        name_entry1 = tk.Entry(self, textvariable = passw_var, font=('Times',20,'normal'))
+        name_entry1.pack(pady=10,padx=10)
 
         confirmation_label = tk.Label(self, text = '', font=('Times',20, 'bold'), fg="red")
         confirmation_label.pack(pady=10,padx=10)
+
+        button = tk.Button(self, text="Submit", 
+            font=('Times', '20'), command=lambda: submitRaket(name_var, passw_var, name_entry, name_entry1, confirmation_label))
+        button.pack(pady=20,padx=20)
 
         ## ADD CODE HERE TO DESIGN THIS PAGE
 
--- admin/utilities.py
+++ admin/utilities.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from datetime import datetime
 import json
+import random
 
 def about():
     messagebox.showinfo('About', "Alexandre Jasmin\n514-553-6209")
@@ -24,3 +25,43 @@ def dump_orders(mydict):#Dumps orders.json
 
 def refresh(self):#Refresh App
     self.refresh()
+
+def checkOrder(client, price):#Check the parameters of the order
+
+    #If name not given?
+    if str(client) == "":
+        messagebox.showinfo('Erreur', "Missing Client Name")
+        return
+    #If price is negative or not a number?
+    try:
+        float(price)
+    except:
+        messagebox.showinfo('Erreur', "Price is NaN")
+        return
+    if float(price) < 0:
+        messagebox.showinfo('Erreur', "Price is < 0$")
+        return
+
+def createOrder(client, price):#Create order dict, returns json
+    #Get order.json
+    orderJSON = get_orders()
+    orderDict = {}
+    #Generate random 6 digit ID
+    order_id = ""
+    for i in range(5):
+        tank = str(random.randint(0,9))
+        order_id = order_id+tank
+    #Get Date
+    date_id = get_date()
+
+    orderDict["client_name"] = client
+    orderDict["order_id"] = order_id
+    orderDict["order_date"] = date_id
+    orderDict["price"] = price
+    orderDict["payment_status"] = False
+    orderJSON.append(orderDict)
+
+    info = {}
+    info["id"] = order_id
+
+    return orderJSON, info
