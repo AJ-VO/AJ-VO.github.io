d7012c78f80d63e75070ebb97018d4b7cf6f0c60
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-11-16 12:39:40 -0500

    Added index information

--- js/script.js
+++ js/script.js
@@ -6,7 +6,11 @@ async function getData() {
     var file = "https://aj-vo.github.io/jsons/players.json";
     let x = await fetch(file);
     let y = await x.text();
-    return y
+
+    var file1 = "https://aj-vo.github.io/jsons/results.json";
+    let z = await fetch(file1);
+    let q = await z.text();
+    return [y, q]
 }
 //send y to functions
 getData().then(
@@ -22,7 +26,6 @@ getData().then(
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
 //on page load
 function pageInitializing() {
     console.log("Page Successfully Initialized");   
@@ -36,18 +39,40 @@ function catchingFetchError(data){
     console.log(data)
 }
 
-//players.json displayer
+//players.json displayer for index.html
 function myDisplayer(data){
     //parse to json
-    const playerData = JSON.parse(data);
+    const playerData = JSON.parse(data[0]),
+        resultsData  = JSON.parse(data[1]);
     //meneur
     var meneurString = "";
-    meneurString = meneurString + "<p>"+playerData[0]["name"]+"</p>";
-    document.getElementById("player_meneur").innerHTML = meneurString;
+    meneurString = meneurString + "&nbsp;"+playerData[0]["name"]+"&nbsp;";
+    document.getElementById("meneur_name").innerHTML = meneurString;
+    meneurString = "";
+    meneurString = meneurString + playerData[0]["elo"] + " ("+playerData[0]["wins"]+"W-"+playerData[0]["losses"]+"L)";
+    document.getElementById("meneur_elo").innerHTML = meneurString;
+    //get last match of meneur
+    for (let i=0; i<resultsData.length;i++){
+        if (resultsData[i]["winner"] == playerData[0]["name"] || resultsData[i]["loser"] == playerData[0]["name"]){
+            document.getElementById("meneur_text").innerHTML = "Dernier Match:<br><b>"+resultsData[i]["winner"]+"</b> d. <b>"+resultsData[i]["loser"]+"</b>";
+            break;
+        }
+    }
     //random
     const randomPlayer = Math.floor(Math.random() * Object.keys(playerData).length);
     var randomString = "";
-    randomString = randomString + "<p>"+playerData[randomPlayer]["name"]+"</p>";
-    document.getElementById("player_random").innerHTML = randomString;
+    randomString = randomString + "&nbsp;"+playerData[randomPlayer]["name"]+"&nbsp;";
+    document.getElementById("random_name").innerHTML = randomString;
+    randomString = "";
+    randomString = randomString + playerData[randomPlayer]["elo"] + " ("+playerData[randomPlayer]["wins"]+"W-"+playerData[randomPlayer]["losses"]+"L)";
+    document.getElementById("random_elo").innerHTML = randomString;
+    //get ranking of random player
+    for (let j=0; j<playerData.length;j++){
+        if (playerData[j]["name"] == playerData[randomPlayer]["name"] || playerData[j]["name"] == playerData[randomPlayer]["name"]){
+            var standing = j+1
+            document.getElementById("random_text").innerHTML = "Classement: #"+(standing);
+            break;
+        }
+    }
 
 }
