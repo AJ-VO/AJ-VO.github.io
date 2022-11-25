97718c9ff3355c1cde64bc65dd1439a35d48c017
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-11-24 22:27:35 -0500

    Created QR

--- utils.py
+++ utils.py
@@ -154,7 +154,7 @@ def dump_json_file(file, myData):#Dump to specific file
 def create_qr_code():#Create QR Code from data
     # Data to be encoded
     data = ""
-    title = "tutorialQR"
+    title = ""
     # Encoding data using make() function
     img = qrcode.make(data)
     # Saving as an image file
