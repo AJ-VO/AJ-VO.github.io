006364ba9a1446701c7661667eeb1892cbeab521
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-11-25 10:29:56 -0500

    updated tutorial

--- js/newTracker.js
+++ js/newTracker.js
@@ -1,43 +1,98 @@
-//functions
+//Main Function
+function start_match(){
 
-//Changes the layout of the buttons on screen
-function button_layout(scene){
-    document.getElementById("button_layer").innerHTML = buttonDict[scene];
+    //containers
+    while (matchOver == false){
+        //while the match is not over
+        while (setOver == false){
+            //while the set is not over
+            while (gameOver == false){
+                //while the game is not over
+                while (pointOver == false){
+                    //while the point is not over
+                    //lastPointWinner must be defined every
+                }//point is over
+                //log event in tracker
+                //edit score
+                currentPoints[lastPointWinner] = currentPoints[lastPointWinner]+1;
+                //change layout
+                button_layout(1);
+                //check result for game is over and tiebreaks
+                if (currentPoints[lastPointWinner] == 7){
+                    //w
+                    gameOver = true;
+                }
+                else if () {
+
+                }
+                else{
+                    //check if end of game
+                }
+                //reset pointOver
+                pointOver = false;
+
+            }//game is over
+            //
+        }//set is over
+        //add set win
+        setWon[lastPointWinner] = setWon[lastPointWinner]+1;
+        //append score
+
+        //check if last winner won the match
+        if (setWon[lastPointWinner] == 2){
+            matchOver = true;
+        }
+
+        //reset setOver status
+        setOver = false;
+
+    }//end of match
+
 }
 
-function start_match(){
-    //start
+//Other Functions
+//Changes the layout of the buttons on screen
+function button_layout(scene){
+    //document.getElementById("button_layer").innerHTML = buttonDict[scene];
 }
 
 //script start
-console.log("tracker.js staring...")
-
+console.log("tracker.js staring...");
 //get players from urls
 var matchup = decodeURIComponent(window.location.search);
 matchup = matchup.substring(2);
 const players = matchup.split("-");
 
-//load starting serve buttons
-button_layout(0);
-
-//all variables
+//initialize all variables
 var trackerBackup;//store tracker for "undo"
-
-//initialize const
+var matchOver, setOver, gameOver, pointOver, setWon, currentPoints, currentGames, lastPointWinner;
+matchOver = setOver = gameOver, pointOver = false;
+setWon, currentPoints = [];
+//const
 const tracker = 0;
+const opp = {
+    0: 1,
+    1: 0
+};
 const pointDict = {
     0: "0",
     1: "15",
     2: "30",
     3: "40",
 };
+//will change during match?
 const buttonDict = {
-    0: "",
+    0: "2 buttons [0 au service, 1 au service]",
     1: "",
     2: "",
     3: "",
     4: ""
 };
-//library
 
-//main functions
+//run
+//load starting serve buttons
+button_layout(0);//choosing the server will trigger start_match()
+
+
+//variables dictionnary
+//setWon = [] = ammount of sets players have won
