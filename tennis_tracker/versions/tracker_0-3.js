47a05ea3b672e9d4ed8ab014b95335db808b70ec
AJ-VO <alexandre.jasmin@usherbrooke.ca>
2022-11-30 12:21:02 -0500

    new tracker update

--- tennis_tracker/versions/tracker_0-3.js
+++ tennis_tracker/versions/tracker_0-3.js
@@ -1,4 +1,6 @@
 //v0.3
+//reset values to past event
+//restart with series of event?
 
 //Request a match tracker
 async function get_tracker() {
@@ -16,9 +18,10 @@ get_tracker().then(
         for (let i=0;i<2;i++){
             myTracker["data"][i]["player_name"] = PLAYERS[i];
         }
+
         //backup initial data
         myTracker["backup_information"]["players"] = PLAYERS;
-        
+
         console.log("got tracker successfully...");
         document.getElementById("status_layer").innerHTML = "Status: Ready";
     },
@@ -29,48 +32,73 @@ get_tracker().then(
 );
 
 //Functions
+function opp_id(id){
+    return 1-id;
+}
+function show_serve_status_ui(id){
+    if (id == playerOnServe){
+        return "*";
+    }
+    else{
+        return "";
+    }
+}
 function backup_data(){
-
+    //edit tracker
+    myTracker["backup_information"]["stats"] = JSON.stringify(myTracker);
+    myTracker["backup_information"]["playerOnServe"] = playerOnServe;
+    //scores
+    myTracker["backup_information"]["players"] = PLAYERS;
+    myTracker["backup_information"]["currentServe"] = currentServe;
+    //momentum
+    //tiebreakStatus
+    //breakpointStatus
 }
 function update_stats(){
 
 }
-function update_score(){
+function update_score(winner){
 
 }
 function check_game_state(){
 
 }
-function button_layout(){
+function button_layout(layout){
 
 }
 function event_manager(nextLayout, event, winner, serve){
 
-    
-
-    update_stats();
-    update_score();
+    //whatever event, backup any data that could be edited
     backup_data();
-    button_layout();
-    check_game_state();
 
+    //if event
+    if (winner == null && serve == null){
+        //
+    }
+    else{
+        update_stats();
+        update_score(winner);
+        check_game_state();
+    }
+
+    //change layout
+    button_layout(nextLayout);
 }
 
 
 //Variables
 var myTracker;
-//get PLAYERS from urls
+//get players from urls/
 var MATCHUP = decodeURIComponent(window.location.search);
 MATCHUP = MATCHUP.substring(2);
 MATCHUP = MATCHUP.split("-");
 //attribute values
 const PLAYERS = [MATCHUP[0], MATCHUP[1]];
 var playerOnServe = MATCHUP[2];
+//variables
+var currentServe = 1;//Current Serve (1,2)
+var start_time = Date.now();//Current time (ms)
 //basic dictionnaries
-const OPP = {
-    0: 1,
-    1: 0
-};
 const pointDict = {
     0: "0",
     1: "15",
@@ -78,4 +106,4 @@ const pointDict = {
     3: "40",
 };
 //start match
-event_manager(0,null,null,null);
+event_manager(0,"start",null,null);
